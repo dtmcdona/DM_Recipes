1b7e6fbf32adffdcd135895dc6c15c31489610d2
dtmcdona <davidmcdwork@gmail.com>
2022-03-26 18:20:14 -0700

    Fixed flake8

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -6,7 +6,6 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the users object"""
-
     class Meta:
         model = get_user_model()
         fields = ('email', 'password', 'name')
